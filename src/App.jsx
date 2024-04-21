caebd5cfaf0e66da780d034871cc132dd2fae8ee
Evan Graham <evangraham19@gmail.com>
2024-04-21 09:00:58 -0700

    input

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,19 @@ import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [celsius, setCelsius] = useState(0);
+  function convertToCelsius(e) {
+    const f = parseFloat(e.target.value);
+    setCelsius((f - 32) * (5 / 9));
+  }
 
   return (
     <>
       <h1>temperature-conversion</h1>
       <div className="card">
+        <label>Fahrenheit:</label>
+        <input type="number" onChange={convertToCelsius}></input>
+        <span>celsius: {celsius.toFixed(2)}</span>
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
